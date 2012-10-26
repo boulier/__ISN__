221418b9dc6204fd2f6885d24b0da4ec82390cfb
Debian Live user <user@localhost.localdomain>
2012-10-26 17:42:43 +0000

    Essai de git

---
+++ Dicho.py
@@ -0,0 +1,26 @@
+#encoding=utf-8
+w="d=plus petit que"
+x="e=plus grand que"
+y="f=bravo!!!"
+
+b=0
+
+d=1
+e=2
+f=3
+
+a=500
+
+
+
+
+
+
+while b!=f:
+	
+	print a
+	b = input("commande")
+	if b==1:
+		a=(a-(a/3)+a)/2
+	if b==2:
+		a=((1000-a)/2)+a
